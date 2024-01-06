36b02ce2fb7ba9fc8b455bed43593f464d2c2fbe
alejandromoreira2002 <tedalemorvel@gmail.com>
2024-01-06 14:01:11 -0500

    Implementacion de limpieza de datos

--- models/MLAlgorithms.py
+++ models/MLAlgorithms.py
@@ -8,7 +8,25 @@ class MLAlgorithms:
         self.cleanData()
     
     def cleanData(self):
-        pass
+        # Eliminacion de variables
+        for column in self.dataCSV:
+            rows = self.dataCSV[column].size
+            nrows = self.dataCSV[column].isnull().sum()
+            perc = (nrows*100)/rows
+            if perc > 60:
+                del self.dataCSV[column]
+
+        # Reemplazo de valores vacios
+        fillnavals = {}
+        for col in self.dataCSV:
+            column = self.dataCSV[col]
+            if column.dtype =='int64':
+                media = column.mean()
+                fillnavals[col] = media
+            elif column.dtype =='O':
+                moda = column.mode()[0]
+                fillnavals[col] = moda
+        self.dataCSV = self.dataCSV.fillna(fillnavals)
 
     def previewData(self):
         # Datos de ejemplo
